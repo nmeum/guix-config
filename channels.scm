74e98da0400d44ca93ea78550b03554b3f89a243
Sören Tempel <soeren+git@soeren-tempel.net>
2025-02-01 00:40:45 +0100

    channels.scm: Initialize

---
+++ channels.scm
@@ -0,0 +1,33 @@
+(list (channel
+        (name 'nonguix)
+        (url "https://gitlab.com/nonguix/nonguix")
+        (branch "master")
+        (commit
+          "5c6ef7cafdfabdacae8ce33410b41168a952d4ff")
+        (introduction
+          (make-channel-introduction
+            "897c1a470da759236cc11798f4e0a5f7d4d59fbc"
+            (openpgp-fingerprint
+              "2A39 3FFF 68F4 EF7A 3D29  12AF 6F51 20A0 22FB B2D5"))))
+      (channel
+        (name 'nmeum)
+        (url "https://github.com/nmeum/guix-channel.git")
+        (branch "master")
+        (commit
+          "7f2e77c69f0b0620fd3422d46088c9f9dc4a59cc")
+        (introduction
+          (make-channel-introduction
+            "808a00792c114c5c1662e8b1a51b90a2d23f313a"
+            (openpgp-fingerprint
+              "514E 833A 8861 1207 4F98  F68A E447 3B6A 9C05 755D"))))
+      (channel
+        (name 'guix)
+        (url "https://git.savannah.gnu.org/git/guix.git")
+        (branch "master")
+        (commit
+          "cd6b88196896e2ff1b6039f97328fa1c5bf1cddf")
+        (introduction
+          (make-channel-introduction
+            "9edb3f66fd807b096b48283debdcddccfea34bad"
+            (openpgp-fingerprint
+              "BBB0 2DDF 2CEA F6A8 0D1D  E643 A2A0 6DF2 A33A 54FA")))))
